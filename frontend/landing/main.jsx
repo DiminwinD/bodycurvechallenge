f139b1075b75645646f09c0fdb60360499fb218d
Lalchimiste <lalchimisteoff@gmail.com>
2026-03-17 22:02:22 +0100

    Fix: redirection externe Chariow

--- frontend/landing/main.jsx
+++ frontend/landing/main.jsx
@@ -1,14 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BodyCurveLanding from "./BodyCurveLanding";
-import Reservation from "./Reservation";
 import Success from "./features/success/Success";
+
+function ExternalRedirect({ to }) {
+  useEffect(() => {
+    window.location.replace(to);
+  }, [to]);
+  return null;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BodyCurveLanding />} />
-        <Route path="/reservation" element={<Navigate to="https://pzptvmid.mychariow.shop/prd_iuoyld" replace />} />
+        <Route path="/reservation" element={<ExternalRedirect to="https://pzptvmid.mychariow.shop/prd_iuoyld" />} />
         <Route path="/success" element={<Success />} />
       </Routes>
     </BrowserRouter>

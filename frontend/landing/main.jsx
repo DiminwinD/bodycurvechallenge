7f3213327d21c05dfd2cc9665ef679d7f9d26ae5
Lalchimiste <lalchimisteoff@gmail.com>
2026-03-17 22:17:10 +0100

    Fix final: ExternalRedirect + catch-all route

--- frontend/landing/main.jsx
+++ frontend/landing/main.jsx
@@ -5,19 +5,30 @@ import Success from "./features/success/Success";
 
 function ExternalRedirect({ to }) {
   useEffect(() => {
-    window.location.replace(to);
+    window.location.href = to;
   }, [to]);
-  return null;
+  return (
+    <div style={{display:"flex",alignItems:"center",
+    justifyContent:"center",height:"100vh",
+    fontFamily:"sans-serif",color:"#666"}}>
+      Redirection en cours...
+    </div>
+  );
 }
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BodyCurveLanding />} />
-        <Route path="/reservation" element={<ExternalRedirect to="https://pzptvmid.mychariow.shop/prd_iuoyld" />} />
         <Route path="/success" element={<Success />} />
+        <Route path="/reservation" element={
+          <ExternalRedirect to="https://pzptvmid.mychariow.shop/prd_iuoyld" />
+        } />
+        <Route path="*" element={<BodyCurveLanding />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
